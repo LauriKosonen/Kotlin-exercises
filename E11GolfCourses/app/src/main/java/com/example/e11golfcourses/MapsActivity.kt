ad92d41540df5d3048aef1e6527964ec70f7d34c
Lauri Kosonen <AB7344@jamk.outlook.fi>
2023-11-20 13:29:34 +0200

    E11 valmis

--- E11GolfCourses/app/src/main/java/com/example/e11golfcourses/MapsActivity.kt
+++ E11GolfCourses/app/src/main/java/com/example/e11golfcourses/MapsActivity.kt
@@ -2,7 +2,14 @@ package com.example.e11golfcourses
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
 
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -10,6 +17,9 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.e11golfcourses.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.Marker
+import org.json.JSONArray
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -40,9 +50,107 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        // Commented out Sydney code
+        // val sydney = LatLng(-34.0, 151.0)
+        // mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        // mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        // Call the loadData function after Google Maps is ready
+        loadData()
+    }
+
+    /**
+     * Load data after Google Maps is ready.
+     */
+    private fun loadData() {
+        val queue = Volley.newRequestQueue(this)
+        // 1. code here
+        val url = "https://ptm.fi/materials/golfcourses/golf_courses.json"
+        var golfCourses: JSONArray
+        var courseTypes: Map<String, Float> = mapOf(
+            "?" to BitmapDescriptorFactory.HUE_VIOLET,
+            "Etu" to BitmapDescriptorFactory.HUE_BLUE,
+            "Kulta" to BitmapDescriptorFactory.HUE_GREEN,
+            "Kulta/Etu" to BitmapDescriptorFactory.HUE_YELLOW
+        )
+
+        // Create JSON request object
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            { response ->
+                // JSON loaded successfully
+                // 2. code here
+                golfCourses = response.getJSONArray("courses")
+                // loop through all objects
+                for (i in 0 until golfCourses.length()){
+                    // get course data
+                    val course = golfCourses.getJSONObject(i)
+                    val lat = course["lat"].toString().toDouble()
+                    val lng = course["lng"].toString().toDouble()
+                    val latLng= LatLng(lat, lng)
+                    val type = course["type"].toString()
+                    val title = course["course"].toString()
+                    val address = course["address"].toString()
+                    val phone = course["phone"].toString()
+                    val email = course["email"].toString()
+                    val webUrl = course["web"].toString()
+
+                    if (courseTypes.containsKey(type)){
+                        val m = mMap.addMarker(
+                            MarkerOptions()
+                                .position(latLng)
+                                .title(title)
+                                .icon(BitmapDescriptorFactory
+                                    .defaultMarker(courseTypes.getOrDefault(type, BitmapDescriptorFactory.HUE_RED)
+                                    )
+                                )
+                        )
+                        // pass data to marker via Tag
+                        val list = listOf(address, phone, email, webUrl)
+                        m!!.tag = list
+                    } else {
+                        Log.d("GolfCourses", "This course type does not exist in evaluation $type")
+                    }
+                }
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(65.5, 26.0),5.0F))
+            },
+            { error ->
+                // Error loading JSON
+            }
+        )
+
+        // Add the request to the RequestQueue
+        queue.add(jsonObjectRequest)
+
+        // 3. Add custom info window adapter here
+        mMap.setInfoWindowAdapter(CustomInfoWindowAdapter())
+    }
+
+    internal inner class CustomInfoWindowAdapter : GoogleMap.InfoWindowAdapter {
+        private val contents: View = layoutInflater.inflate(R.layout.info_window, null)
+
+        override fun getInfoWindow(marker: Marker): View? {
+            return null
+        }
+
+        override fun getInfoContents(marker: Marker): View {
+            // UI elements
+            val titleTextView = contents.findViewById<TextView>(R.id.titleTextView)
+            val addressTextView = contents.findViewById<TextView>(R.id.addressTextView)
+            val phoneTextView = contents.findViewById<TextView>(R.id.phoneTextView)
+            val emailTextView = contents.findViewById<TextView>(R.id.emailTextView)
+            val webTextView = contents.findViewById<TextView>(R.id.webTextView)
+            // title
+            titleTextView.text = marker.title.toString()
+            // get data from Tag list
+            if (marker.tag is List<*>){
+                val list: List<String> = marker.tag as List<String>
+                addressTextView.text = list[0]
+                phoneTextView.text = list[1]
+                emailTextView.text = list[2]
+                webTextView.text = list[3]
+            }
+            return contents
+        }
     }
 }
